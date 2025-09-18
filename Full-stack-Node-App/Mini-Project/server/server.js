eb275b57161bfd573ca55238602a0b49406ebe24
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-09-18 15:06:45 +0530

    Fs Modules and serving html

--- Full-stack-Node-App/Mini-Project/server/server.js
+++ Full-stack-Node-App/Mini-Project/server/server.js
@@ -1,18 +1,25 @@
 import http from 'node:http'
 import path from 'node:path'
-import { testPath } from '../utils/testPath.js'
-const PORT = 8000
+// import { testPath } from '../utils/testPath.js'
+import { fileURLToPath } from 'node:url'
+import fs from 'node:fs/promises'
 
-const __dirname = import.meta.dirname
+const PORT = 2000
 
-const server = http.createServer((req, res)=> {
-  const absPathToResource = path.join(__dirname, "node-express-journey", "Full-stack-Node-App","Mini-Project" , "public", 'index.html')
-  const relPathToResource = path.join("node-express-journey", "Full-stack-Node-App","Mini-Project" , "public", 'index.html')
-  console.log(absPathToResource);
-  console.log(relPathToResource);
-  testPath(__dirname)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const server = http.createServer(async(req, res)=> {
+//   const absPathToResource = path.join(__dirname, "node-express-journey", "Full-stack-Node-App","Mini-Project" , "public", 'index.html')
+//   const relPathToResource = path.join("node-express-journey", "Full-stack-Node-App","Mini-Project" , "public", 'index.html')
+//   console.log(absPathToResource);
+//   console.log(relPathToResource);
+//   testPath(__dirname)
+  const pathToResource = path.join(__dirname,"../public/index.html")
+  const content = await fs.readFile(pathToResource)
+  res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end()
+  res.end(content)
 })
 
-server.listen(PORT, () => console.log('connected on port 8000'))
+server.listen(PORT, () => console.log(`connected on port: ${PORT}`))
