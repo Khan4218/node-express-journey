ccdcaaddd07d71b7c3dce2aeb4ba9faf2f64fc99
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-09-20 19:38:06 +0530

    Handling POST 2

--- Full-stack-Node-App/Main-Project/utils/addNewSighting.js
+++ Full-stack-Node-App/Main-Project/utils/addNewSighting.js
@@ -1,3 +1,20 @@
-export async function addNewSighting() {
+import path from "node:path";
+import fs from "node:fs/promises"
+import { getData } from "./getData.js";
+import { fileURLToPath } from "node:url";
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const sightings = await getData()
+
+export async function addNewSighting(newSighting) {
+  try {
+    sightings.push(newSighting)
+    const pathToJson = path.join(__dirname, "../data", "data.json")
+    await fs.writeFile(pathToJson, JSON.stringify(sightings, null, 2), "utf8")
+    
+    
+  } catch (err) {
+    throw new Error(err)
+  }
 }
