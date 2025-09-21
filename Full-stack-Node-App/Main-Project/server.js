f306237fba86c9f7cffaf790b3a137885aafc6a9
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-09-21 19:32:00 +0530

    Deploying my project

--- Full-stack-Node-App/Main-Project/server.js
+++ Full-stack-Node-App/Main-Project/server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url'
 import { getData } from './utils/getData.js'
 import { handleGet, handlePost, handleNews} from './handlers/handleRoutes.js'
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
