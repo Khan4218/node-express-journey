5a2a37732213fa79e9a9aa717f7a41b849196eb3
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-09-21 19:19:40 +0530

    Implement server-sent-events in main project

--- Full-stack-Node-App/Main-Project/server.js
+++ Full-stack-Node-App/Main-Project/server.js
@@ -1,15 +1,15 @@
 import http from 'node:http'
 import path from 'node:path'
-import { serveStatic } from '../utils/serveStatic.js'
+import { serveStatic } from './utils/serveStatic.js'
 import { fileURLToPath } from 'node:url'
-import { getData } from '../utils/getData.js'
-import { handleGet, handlePost } from '../handlers/handleRoutes.js'
+import { getData } from './utils/getData.js'
+import { handleGet, handlePost, handleNews} from './handlers/handleRoutes.js'
 
 const PORT = 8000
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const rootDir = path.join(__dirname, '../public')
+const rootDir = path.join(__dirname, './public')
 
 
 const server = http.createServer(async (req, res) => {
@@ -18,11 +18,12 @@ const server = http.createServer(async (req, res) => {
   }
   else if(req.url === "/api" && req.method === "POST") {
    await handlePost(req, res)
+  }else if (req.url.startsWith("/api/news")) {
+   return await handleNews(req,res)
   }
   else if(!req.url.startsWith('/api')){
     return await serveStatic(req, res, rootDir)
   }
-  
    
 }) 
 
